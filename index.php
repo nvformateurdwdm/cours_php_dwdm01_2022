d17ed6ad5d93d80821f2a6ce2d81d79cc7389c24
nvformateurdwdm <nvformateurdwdm@gmail.com>
2022-07-19 10:26:09 -0400

    Déclaration de variable.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Cours</title>
+</head>
+<body>
+    <?php 
+    
+        $nom = "VEDRINE";
+        $prenom = "Nicolas";
+
+        echo $nom;
+        echo "<br>";
+        echo $prenom;
+    
+    ?>
+</body>
+</html>
