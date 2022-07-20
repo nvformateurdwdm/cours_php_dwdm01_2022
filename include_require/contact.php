80e0b23930298eb059cd877498d6a0fe0b1efb1e
nvformateurdwdm <nvformateurdwdm@gmail.com>
2022-07-20 13:50:14 -0400

    isset

--- include_require/contact.php
+++ include_require/contact.php
@@ -9,9 +9,13 @@
 
         include "paragraphe.php";
 
-        // echo "<h1>Bonjour, je m'appelle " . $_GET["prenom"] . " " . $_GET["nom"] . "." . "<br>J'ai " . $_GET["age"] . " ans." . "</h1>";
-        
-        echo "<h1>Bonjour, je m'appelle " . $_POST["prenom"] . " " . $_POST["nom"] . "." . "</h1>";
+        if(isset($_GET["prenom"]) && isset($_GET["nom"]) && isset($_GET["age"])){
+            echo "<h1>Bonjour, je m'appelle " . $_GET["prenom"] . " " . $_GET["nom"] . "." . "<br>J'ai " . $_GET["age"] . " ans." . "</h1>";
+        }
+
+        if(isset($_POST["prenom"])){
+            echo "<h1>Bonjour, je m'appelle " . $_POST["prenom"] . " " . $_POST["nom"] . "." . "</h1>";
+        }
         
     ?>
 </body>
